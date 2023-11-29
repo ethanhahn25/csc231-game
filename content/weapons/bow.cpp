911dd1a053ca03b5e3680969d3ee4d5b7a2b9937
ethanhahn <ethan.hahn@mail.wlc.edu>
2023-11-29 13:33:09 -0600

    fixed typo in bow

--- content/weapons/bow.cpp
+++ content/weapons/bow.cpp
@@ -7,7 +7,8 @@
 Bow::Bow(int damage)
     :Weapon{"bow", damage}{}
 
-void Bow::use(Engine& engine, Entity& attacker, Entity& defender){
+void Bow::use(Engine& engine, Entity& attacker, Entity& defender) {
     Vec direction = defender.get_position() - attacker.get_position();
     std::shared_ptr<Event> thrust = engine.events.create_event<Thrust>(sprite, direction);
     thrust->add_next(Hit{defender, damage});
+}
